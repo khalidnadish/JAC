d954e527fd89e81b52264ed4a97fff587813e008
khalidnadish <w2.nadish@gmail.com>
2022-11-24 14:27:32 +0300

    prduct pages

--- src/pages/about/Prodct.jsx
+++ src/pages/about/Prodct.jsx
@@ -14,11 +14,24 @@ export default function Prodct() {
   return (
     <>
       <cpm.CardWrapper>
-        <MyCard  image={"/assets/product/H-153243-1-5.jpg"} title={t("h84.title")} subtitle={t("m84.subtitle")} description={t("m84.description")} />
-        <MyCard  image={"/assets/product/H-153245-1-6.jpg"} title={t("h45.title")} subtitle={t("h45.subtitle")} description={t("m84.description")} />
-        <MyCard  image={"/assets/product/H-153247-1-7.jpg"} title={t("h47.title")} subtitle={t("h47.subtitle")} description={t("m84.description")} />
-        <MyCard  image={"/assets/product/H-153259-1-8.jpg"} title={t("h59.title")} subtitle={t("h59.subtitle")} description={t("m84.description")} />
-        <MyCard  image={"/assets/product/H-153273-1-9.jpg"} title={t("h73.title")} subtitle={t("h73.subtitle")} description={t("m84.description")} />
+      <cpm.SectionTitle>{t("dezel")}</cpm.SectionTitle>
+        <MyCard  image={"/assets/product/H-153243-1-5.jpg"} title={t("D1.title")} subtitle={t("D1.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153245-1-6.jpg"} title={t("D2.title")} subtitle={t("D2.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153247-1-7.jpg"} title={t("D3.title")} subtitle={t("D3.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153259-1-8.jpg"} title={t("D4.title")} subtitle={t("D4.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153273-1-9.jpg"} title={t("D5.title")} subtitle={t("D5.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153243-1-5.jpg"} title={t("D6.title")} subtitle={t("D6.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153245-1-6.jpg"} title={t("D7.title")} subtitle={t("D7.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153247-1-7.jpg"} title={t("D8.title")} subtitle={t("D8.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153259-1-8.jpg"} title={t("D9.title")} subtitle={t("D.subtitle")}  description={t("m84.description")} />
+        <cpm.SectionTitle>{t("electric")}</cpm.SectionTitle>
+        <MyCard  image={"/assets/product/H-153273-1-9.jpg"} title={t("E1.title")} subtitle={t("E1.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153243-1-5.jpg"} title={t("E2.title")} subtitle={t("E2.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153245-1-6.jpg"} title={t("E3.title")} subtitle={t("E3.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153247-1-7.jpg"} title={t("E4.title")} subtitle={t("E4.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153259-1-8.jpg"} title={t("E5.title")} subtitle={t("E5.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153273-1-9.jpg"} title={t("E6.title")} subtitle={t("E6.subtitle")} description={t("m84.description")} />
+        <MyCard  image={"/assets/product/H-153273-1-9.jpg"} title={t("E7.title")} subtitle={t("E7.subtitle")} description={t("m84.description")} />
         {/* <MyCard  image={"/assets/product/H-153243-1-5.jpg"}/> */}
         
       </cpm.CardWrapper>
@@ -35,7 +48,7 @@ const MyCard =({image,title,subtitle,description}) => {
   const {t} =useTranslation()
   return(<>
 
-<Card sx={{ maxWidth: 300 }}>
+<Card sx={{ maxWidth: 300 ,minWidth:300 }}>
       <CardActionArea>
         <CardMedia
           component="img"
--- src/pages/about/aboutCpm.jsx
+++ src/pages/about/aboutCpm.jsx
@@ -1,45 +1,52 @@
 import styled from "styled-components";
 
-const Wraper =styled.div``;
+const Wraper = styled.div``;
 /* ------------------------------------------- */
-const ImageWraper =styled.div`
-display: flex;
-align-items: end;
-justify-content: flex-end;
-overflow:auto; 
-width:100%;
-/* height: 10%; */
-border: .5px solid rgba(255,255,255,0.1);
-padding:7px;
-border-radius:8px;
+const ImageWraper = styled.div`
+  display: flex;
+  align-items: end;
+  justify-content: flex-end;
+  overflow: auto;
+  width: 100%;
+  /* height: 10%; */
+  border: 0.5px solid rgba(255, 255, 255, 0.1);
+  padding: 7px;
+  border-radius: 8px;
 `;
 /* ------------------------------------------- */
-const Image =styled.img`
-width: 100%;
-height:auto;
-border-radius:8px;
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
 `;
 /* ------------------------------------------- */
-const Txt= styled.p`
-text-align:${({lng})=> lng === "ar" ? "right" : "left"} ;
-margin-top: 1.5em;
-padding: 1rem;
-font-family :'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;
-font-family:${({lng})=> lng === "ar" ? "tr" : 'Segoe UI'} ;
+const Txt = styled.p`
+  text-align: ${({ lng }) => (lng === "ar" ? "right" : "left")};
+  margin-top: 1.5em;
+  padding: 1rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-family: ${({ lng }) => (lng === "ar" ? "tr" : "Segoe UI")};
 `;
 /* ------------------------------------------- */
-const CardWrapper= styled.div`
-display: flex;
-flex-direction: row;
-justify-content:space-around;
-align-items:center;
-flex-wrap: wrap;
-gap: 1rem;
-@media (max-width: 500px) {
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+  @media (max-width: 500px) {
     flex-direction: column;
   }
-
-
+ 
 
 `;
-export default {Txt,Wraper,ImageWraper,Image,CardWrapper}
+/* ---------------------------------------------- */
+const SectionTitle= styled.div`
+width: 100%;
+background-color: #f79335;
+color: black;
+border-radius: 8px;
+`;
+
+export default { Txt, Wraper, ImageWraper, Image, CardWrapper,SectionTitle };
